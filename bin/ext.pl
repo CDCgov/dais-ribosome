207c05c99fe720476e7c6e27d156d3e886107443
Samuel Shepard <vfn4@cdc.gov>
2020-04-29 15:38:57 -0400

    Updated input checking for BETACORONAVIRUS

--- bin/ext.pl
+++ bin/ext.pl
@@ -2,13 +2,24 @@
 # SSS - 2019
 
 
-$/ = "\n";
-$L = <>;
-chomp($L);
 
-$ID = '\w+';
-$annot = '[ABC](_[A-Z0-9]+){1,2}';
-$seq = '[a-zA-Z.~-]+';
+if ( scalar(@ARGV) != 2 ) {
+	die("$0 <input> <MODULE>\n\n");
+}
+
+open(IN,'<',$ARGV[0]) or die("Cannot open $ARGV[0].\n");
+$/ = "\n";
+my $module = $ARGV[1];
+my $L = <IN>; chomp($L);
+close(IN);
+
+my $ID = '\w+';
+my $annot = '[ABC](_[A-Z0-9]+){1,2}';
+my $seq = '[a-zA-Z.~-]+';
+
+if ( $module =~ /CORONAVIRUS/i ) {
+	$annot = '[A-Z]+-CoV(-\w+)*';
+}
 
 if ( $L =~ /^$ID\t$annot\t$seq$/ ) {
 	print "atxt";
