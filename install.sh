f8c4f5ad3f762a987a88dab1b2c7729d79cd2230
Samuel Shepard <vfn4@cdc.gov>
2026-06-22 11:51:24 -0400

    Update install script, ignore, version

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+app=ribosome
+
+command -v "cargo" > /dev/null 2>&1 || {
+    echo "ERROR: you may need to install Rust nightly!"
+    exit 1
+}
+
+if [ -n "$1" ]; then
+    if grep -Eq "not a recognized processor" <(rustc -C "target-cpu=$1" --print host-tuple 2>&1); then
+        echo "WARNING: cannot find target-cpu '$1', skipping"
+    else
+        export RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-C target-cpu=$1"
+    fi
+fi
+
+cargo build --profile prod \
+    && cp target/prod/$app . \
+    && ./.package-sswsort.sh \
+    || {
+        echo "ERROR: Installation failed!"
+        exit 1
+    }
