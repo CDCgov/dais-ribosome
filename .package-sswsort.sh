ba392a0cf678c9fda46765ffa94c598fc858c54a
Samuel Shepard <vfn4@cdc.gov>
2026-06-30 12:36:24 -0400

    Update data pull for SSWSORT resource

--- .package-sswsort.sh
+++ .package-sswsort.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-default_sswsort=v2.2.2
+default_sswsort=v2.2.4
 
 version=${PINNED_SSWSORT:-$default_sswsort}
 folder=sswsort-${version#v}
 archive=${folder}.zip
 url=https://github.com/CDCgov/sswsort/archive/refs/tags/${version}.zip
 
-wget "$url" --output-document "$archive" \
+curl -L --proto '=https' --tlsv1.2 -sSf --output "$archive" "$url" \
     && unzip -o "$archive" \
     && rm "$archive" \
     && mv "${folder}/sswsort_res" . \
